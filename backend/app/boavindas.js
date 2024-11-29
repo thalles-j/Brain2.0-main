6e8220b7fdf263806c957d0d9293b96ba6088340
Thalles José <thallesjosesv@gmail.com>
2024-11-29 09:50:55 -0300

    uptade 2.024

--- backend/app/boavindas.js
+++ backend/app/boavindas.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const nomeUsuario = sessionStorage.getItem('usuarioLogado');
-
     const contentBoasVindas = document.getElementById('content-boas-vindas');
 
     if (nomeUsuario) {
-        contentBoasVindas.innerHTML = `<p>Seja bem-vindo, <strong>${nomeUsuario}</strong>! Este é um espaço seguro onde você pode compartilhar suas emoções e experiências. Estamos aqui para ouvir você e apoiar seu bem-estar. 💙</p>`;
+        
+        const nomeTruncado = nomeUsuario.length > 15 
+            ? nomeUsuario.substring(0, 15) + '...' 
+            : nomeUsuario;
+
+        contentBoasVindas.innerHTML = `<p>Seja bem-vindo, <strong>${nomeTruncado}</strong>! Este é um espaço seguro onde você pode compartilhar suas emoções e experiências. Estamos aqui para ouvir você e apoiar seu bem-estar. 💙</p>`;
     } else {
         contentBoasVindas.innerHTML = `<p>Seja bem-vindo à nossa comunidade! Aqui, você encontrará um espaço acolhedor para desabafar, compartilhar sentimentos e se conectar com pessoas que entendem o que você está passando. Juntos, somos mais fortes! 🌱</p>`;
     }
-    
 });
